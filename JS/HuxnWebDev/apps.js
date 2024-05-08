84d86dbee3b09d0b63cd6cab30ac95cc1c09cc00
riski <duriskifeb@gmail.com>
2024-05-08 22:02:11 +0700

    Menambahkan perubahan pada folder JS/HuxnWebDev/

---
+++ JS/HuxnWebDev/apps.js
@@ -0,0 +1,37 @@
+// let name = "riski febriyanto";
+// let WahtDoyouWannaBecomeInYourLife = "Proagamer";
+// let gender = "male";
+// let twitterhandle = "@duriskifeb";
+
+// console.log(name);
+// console.log(WahtDoyouWannaBecomeInYourLife);
+// console.log(gender);
+// console.log(twitterhandle);
+
+// let number = 100;
+
+// console.log(number + undefined); // NaN
+
+// let fristName = "Alex";
+// let latsName = "Kocak gaming";
+
+// let fullName = `${fristName} ${latsName} something`;
+// console.log(fullName);
+
+
+// let PemeranPertama = "Brucc lee";
+// let PemeranKedua = "Jackie chan";
+// let fullname = PemeranPertama + " " + PemeranKedua;
+// let uppercase = fullname.toUpperCase(); 
+
+// let message = `jangan malas untuk menuliskan sebuah code yaa,dan juga jangan lupa sholat...${uppercase}, `
+// message += `his best tv show is silicion valley` 
+// console.log(message);
+
+// let password = 9;
+
+// if (password === 9) {
+//     console.log("Welcome, Succesffully.")
+// }else if {
+//     console.log("look at this")
+// }
