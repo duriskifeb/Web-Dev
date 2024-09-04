0eacb0760e67ea43bdffc53d30f2912e58a0919d
Traveliki- <https://github.com/duriskifeb>
2024-09-04 14:28:27 +0700

    Update web coffe

--- coffe/js/script.js
+++ coffe/js/script.js
@@ -1,5 +1,6 @@
+// Toggle class active untuk hamburger menu
 const navbarNav = document.querySelector(".navbar-nav");
-// ketika hamburger menu di klik bebas mau keluar 
+// ketika hamburger menu di klik
 document.querySelector("#hamburger-menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
